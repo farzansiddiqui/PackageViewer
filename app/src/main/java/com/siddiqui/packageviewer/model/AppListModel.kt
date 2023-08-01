ed92109d77e738759919fbc1806a499d61a7dedc
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-08-02 00:28:13 +0530

    file updated...

--- app/src/main/java/com/siddiqui/packageviewer/model/AppListModel.kt
+++ app/src/main/java/com/siddiqui/packageviewer/model/AppListModel.kt
@@ -1,5 +1,4 @@
 package com.siddiqui.packageviewer.model
 
-import android.graphics.drawable.Drawable
 
 data class AppListModel(var applicationName:String,var packageName:String,var imageDrawable:Int)
